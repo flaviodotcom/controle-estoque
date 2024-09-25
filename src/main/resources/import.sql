2faf4b5840f411308052f97269464016b4a9d024
Flavio <flavioluiscarneiro@gmail.com>
2024-09-25 17:30:53 -0300

    test: testando todos os endpoints de ProdutoResource

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,9 @@
 -- The commands are commented as their support depends of the database
 insert into produto (id, nome, preco, quantidade, data_validade)
 values (203, 'Pão', 1.00, 1, current_date + 1);
+
+insert into produto (id, nome, preco, quantidade, data_validade)
+values (204, 'Refrigerante', 10.00, 1, current_date + 1);
+
+insert into produto (id, nome, preco, quantidade, data_validade)
+values (205, 'Pão de Queijo', 2.00, 1, current_date - 1);
--- src/test/java/com/github/estoque/ProdutoResourceTest.java
+++ src/test/java/com/github/estoque/ProdutoResourceTest.java
@@ -1,10 +1,14 @@
 package com.github.estoque;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -13,13 +17,21 @@ import static org.hamcrest.core.IsNull.notNullValue;
 @QuarkusTest
 class ProdutoResourceTest {
 
+    private final String jsonBody = """
+            {
+                "nome": "Refrigerante",
+                "preco": 8.0,
+                "quantidade": 1.0
+            }""";
+
     @BeforeAll
     static void setup() {
         RestAssured.defaultParser = Parser.JSON;
     }
 
     @Test
-    void testProdutoEndpoint() {
+    @TestSecurity(authorizationEnabled = false)
+    void testGetProdutoById() {
         given()
                 .when().get("/estoque/203")
                 .then()
@@ -30,4 +42,73 @@ class ProdutoResourceTest {
                 .body("quantidade", is(1))
                 .body("dataValidade", notNullValue());
     }
+
+    @Test
+    @TestSecurity(user = "jose", roles = {"admin", "user"})
+    void testGetAllProdutos() {
+        given()
+                .when().get("/estoque")
+                .then()
+                .statusCode(200)
+                .body("size() > 0", is(true));
+    }
+
+    @Test
+    @TestSecurity(user = "jose", roles = {"admin", "user"})
+    void testCadatrarProdutoComAutorizacao() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(jsonBody)
+                .when().post("/estoque")
+                .then().statusCode(201);
+    }
+
+    @Test
+    @TestSecurity(user = "alice", roles = "user")
+    void testCadastrarProdutoSemAutorizacao() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(jsonBody)
+                .when().post("/estoque")
+                .then().statusCode(403);
+    }
+
+    @Test
+    @TestSecurity(user = "jose", roles = {"admin", "user"})
+    void testAtualizarProdutoComAutorizacao() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(jsonBody)
+                .when().put("/estoque/204")
+                .then().statusCode(204);
+    }
+
+    @Test
+    @TestSecurity(user = "alice", roles = "user")
+    void testAtualizarProdutoSemAutorizacao() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(jsonBody)
+                .when().put("/estoque/204")
+                .then().statusCode(403);
+    }
+
+    @Test
+    @TestSecurity(user = "alice", roles = "user")
+    void testProdutosQueEstaoPertoDoVencimento() {
+        given()
+                .when().get("/estoque/vencimento")
+                .then().statusCode(200)
+                .body("nome.toString().contains(\"Pão\")", is(true));
+    }
+
+    @Test
+    @TestSecurity(user = "alice", roles = "user")
+    void testProdutosQueEstaoVencidos() {
+        given()
+                .when().get("/estoque/vencidos")
+                .then().statusCode(200)
+                .body("nome", is(List.of("Pão de Queijo")));
+    }
+
 }
